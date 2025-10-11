ed3c1ebf37e286548e0e89755d361b94c09af457
eduardo106919 <a106919@alunos.uminho.pt>
2025-10-11 15:25:12 +0100

    improved code for qp03

--- fichas/ficha-03/QP03.hs
+++ fichas/ficha-03/QP03.hs
@@ -1,19 +1,18 @@
+
 module QP03 where
+
 
 import Cp
 import Data.List
 
+
 type Key = String
-
 type Aut = String
-
 type Pag = Int
-
 type Bib = [(Key, [Aut])]
-
 type Aux = [(Pag, [Key])]
-
 type Ind = [(Aut, [Pag])]
+
 
 testBib :: Bib
 testBib =
@@ -40,90 +39,76 @@ testAux =
     (12, ["K_HU"])
   ]
 
+
 mkInd :: (Bib, Aux) -> Ind
 mkInd = map (id >< sort) . uncurry applyMapping . swap . (invertRelation >< invertRelation)
 
--- | Swaps the key-value relationship in a list of pairs of lists.
---
--- Each pair @(a, [b])@ is expanded so that each @b@ becomes a key,
--- associated with all @a@s that pointed to it in the original list.
---
--- == Example:
---
--- >>> invertRelation [("x", [1,2]), ("y", [2,3])]
--- [(1,["x"]),(2,["x","y"]),(3,["y"])]
+{- | Swaps the key-value relationship in a list of pairs of lists.
+
+Each pair @(a, [b])@ is expanded so that each @b@ becomes a key,
+associated with all @a@s that pointed to it in the original list.
+
+== Example:
+
+>>> invertRelation [("x", [1,2]), ("y", [2,3])]
+[(1,["x"]),(2,["x","y"]),(3,["y"])]
+-}
 invertRelation :: (Ord a, Ord b) => [(a, [b])] -> [(b, [a])]
 invertRelation = groupPairs . sortOn p1 . map swap . expandPairs
 
--- | Expands each pair @(a, [b])@ into multiple pairs @(a, b)@,
---     one for each element of the list.
---
--- == Example:
---
--- >>> expandPairs [("x", [1,2]), ("y", [3])]
--- [("x",1),("x",2),("y",3)]
+{- | Expands each pair @(a, [b])@ into multiple pairs @(a, b)@,
+     one for each element of the list.
+
+== Example:
+
+>>> expandPairs [("x", [1,2]), ("y", [3])]
+[("x",1),("x",2),("y",3)]
+-}
 expandPairs :: [(a, [b])] -> [(a, b)]
 expandPairs = concat . map (\(x, l) -> map (x,) l)
 
--- | Compresses a list of pairs into a list of grouped pairs.
---
--- Pairs with the same first element are grouped together,
--- and their second components are collected into a list.
---
--- The order of the input list determines grouping behavior.
---
--- == Example:
---
--- >>> groupPairs [("x",1),("x",2),("y",3)]
--- [("x",[1,2]),("y",[3])]
-groupPairs :: (Eq a) => [(a, b)] -> [(a, [b])]
-groupPairs =
-  uncurry zip
-    . split
-      (nub . map p1)
-      (map (map p2) . groupBy (curry (uncurry (==) . (p1 >< p1))))
+{- | Compresses a list of pairs into a list of grouped pairs.
 
--- | Replaces values in a list of associations according to a mapping.
---
--- Given a dictionary @[(b, [c])]@ and a list @[(a, [b])]@,
--- every @b@ in the second list is replaced by its corresponding
--- list of @c@s from the first list (concatenated if multiple match).
---
--- == Example:
---
--- >>> applyMapping [("x", [1,2]), ("y", [3])] [("a", ["x"]), ("b", ["y","x"])]
--- [("a",[1,2]),("b",[3,1,2])]
+Pairs with the same first element are grouped together,
+and their second components are collected into a list.
+
+The order of the input list determines grouping behavior.
+
+== Example:
+
+>>> groupPairs [("x",1),("x",2),("y",3)]
+[("x",[1,2]),("y",[3])]
+-}
+groupPairs :: (Eq a) => [(a, b)] -> [(a, [b])]
+groupPairs = uncurry zip . split (nub . map p1) 
+                                 (map (map p2) . groupBy (curry (uncurry (==) . (p1 >< p1))))
+
+{- | Replaces values in a list of associations according to a mapping.
+
+Given a dictionary @[(b, [c])]@ and a list @[(a, [b])]@,
+every @b@ in the second list is replaced by its corresponding
+list of @c@s from the first list (concatenated if multiple match).
+
+== Example:
+
+>>> applyMapping [("x", [1,2]), ("y", [3])] [("a", ["x"]), ("b", ["y","x"])]
+[("a",[1,2]),("b",[3,1,2])]
+-}
 applyMapping :: (Eq b) => [(b, [c])] -> [(a, [b])] -> [(a, [c])]
 applyMapping a = map (applyMappingOne a)
 
--- | Helper for 'applyMapping'. Replaces all elements of the second component
---     of a pair according to a lookup table.
---
--- If an element is not found in the dictionary, it contributes nothing.
---
--- == Example:
---
--- >>> applyMappingOne [("x",[1]),("y",[2,3])] ("a", ["y","z","x"])
--- ("a",[2,3,1])
+{- | Helper for 'applyMapping'. Replaces all elements of the second component
+     of a pair according to a lookup table.
+
+If an element is not found in the dictionary, it contributes nothing.
+
+== Example:
+
+>>> applyMappingOne [("x",[1]),("y",[2,3])] ("a", ["y","z","x"])
+("a",[2,3,1])
+-}
 applyMappingOne :: (Eq b) => [(b, [c])] -> (a, [b]) -> (a, [c])
-applyMappingOne d =
-  id
-    >< ( concat
-           . map
-             ( \x -> case lookup x d of
-                 Just xs -> xs
-                 _ -> []
-             )
-       )
-
-
-pages :: Key -> [Pag]
-pages = undefined
-
-keys :: Aut -> [Key]
-keys = undefined
-
-auths :: Bib -> [Aut]
-auths = nub . concat . map snd
-
+applyMappingOne d = id >< (concat . map (\ x -> case lookup x d of
+                                                  Just xs -> xs
+                                                  _ -> []))
 
